b349855ded26a7c44179d07bbfbc83e64c868164
Tony van der Peet <tony.vanderpeet@alliedtelesis.co.nz>
2024-09-26 14:20:34 +1200

    post: fix comparison for If-Unmodified-Since header
    Comparison was != instead of >. Added a test to show it's fixed.

--- tests/test_replace.py
+++ tests/test_replace.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import time
+import re
 from conftest import server_uri, server_auth, docroot, apteryx_set, apteryx_get, apteryx_traverse, get_restconf_headers, set_restconf_headers
 
 
@@ -40,13 +41,16 @@ def test_restconf_replace_list_entry_exists():
 
 
 def test_restconf_replace_if_not_modified_since():
+    """
+    Using last modified from get will fail when we modify the variable and try a put with If-Unmodified-Since.
+    """
     response = requests.get("{}{}/data/test/settings/priority".format(server_uri, docroot), auth=server_auth, headers=get_restconf_headers)
     assert response.status_code == 200 and len(response.content) > 0 and response.json() == json.loads('{ "priority": 1 }')
     last_modified = response.headers.get("Last-Modified")
     time.sleep(1)
     apteryx_set("/test/settings/priority", "2")
     headers = {**get_restconf_headers, 'If-Unmodified-Since': last_modified}
-    response = requests.put("{}{}/data/test/settings".format(server_uri, docroot), auth=server_auth, headers=headers, data="""{"priority": "3"}""")
+    response = requests.put("{}{}/data/test/settings/priority".format(server_uri, docroot), auth=server_auth, headers=headers, data="""{"priority": "3"}""")
     assert response.status_code == 412
     assert len(response.content) > 0
     print(json.dumps(response.json(), indent=4, sort_keys=True))
@@ -65,6 +69,38 @@ def test_restconf_replace_if_not_modified_since():
 }
     """)
     assert apteryx_get("/test/settings/priority") == "2"
+
+
+def test_restconf_replace_if_not_modified_since_2():
+    """
+    Using last modified adjusted one second past the time from the get will work with PUT and If-Unmodified-Since.
+    This test was added because original code used != instead of > to check timestamps, so we need a time that is
+    not equal.
+    """
+    response = requests.get("{}{}/data/test/settings/priority".format(server_uri, docroot), auth=server_auth, headers=get_restconf_headers)
+    assert response.status_code == 200 and len(response.content) > 0 and response.json() == json.loads('{ "priority": 1 }')
+    last_modified = response.headers.get("Last-Modified")
+
+    # Adjust last_modified one second into the future. If we run this test at 23:59:59 it's an automatic pass.
+    xxx = re.match("^(.*)([0-9][0-9]):([0-9][0-9]):([0-9][0-9])(.*)$", last_modified)
+    sec = int(xxx.group(4))
+    minu = int(xxx.group(3))
+    hour = int(xxx.group(2))
+    sec += 1
+    if sec == 60:
+        minu += 1
+        sec = 0
+        if minu == 60:
+            hour += 1
+            minu = 0
+            if hour == 24:
+                return
+    time.sleep(1.1)
+    last_modified = f"{xxx.group(1)}{hour:02}:{minu:02}:{sec:02}{xxx.group(5)}"
+    headers = {**get_restconf_headers, 'If-Unmodified-Since': last_modified}
+    response = requests.put("{}{}/data/test/settings/priority".format(server_uri, docroot), auth=server_auth, headers=headers, data="""{"priority": 3}""")
+    assert response.status_code == 204
+    assert apteryx_get("/test/settings/priority") == "3"
 
 
 def test_restconf_replace_if_not_modified_since_namespace():
